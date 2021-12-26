ce1bc57e85c4ac7c08c48c4d04a199126aa866b1
Hariom <harim987316@gmail.com>
2021-12-26 21:15:59 +0530

    template check-in

---
+++ dependency/app.js
@@ -0,0 +1,93 @@
+/**
+ * Creating main content
+ */
+const _IntroductionToCSS = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _ConditionalOperators = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _Function = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _ScopeBlock = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _Array = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _Loop = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _HigherOrderFunction = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _Iterators = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _Object = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _AdvanceObject = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _Classes = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _Transpilation = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _ModuleExportImport = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _Promises = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _AsyncAwait = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _RequestGetPost = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _RequestFetch = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
+const _WhatNext = () => {
+  let _pre = document.createElement("pre");
+  _pre.innerHTML = ``;
+  return _pre;
+};
