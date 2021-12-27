1e8f9657ec15ae1db09a8d2c922baabaf38b04f0
Hariom <harim987316@gmail.com>
2021-12-27 16:12:26 +0530

    check-in change

--- app.js
+++ app.js
@@ -2917,6 +2917,48 @@ const _WhatNext = () => {
 };
 
 /**
+ * Creating Landing page
+ */
+const _landupContent = (content) => {
+  let _main = document.getElementsByClassName("main")[0];
+  let _div = document.createElement("div");
+  _div.id = content;
+  _div.className = "maincontent";
+  let _h1 = document.createElement("h1");
+  _h1.id = "CSS";
+  let _ndiv = document.createElement("div");
+  _ndiv.style =
+    "text-align: center; background-color: #404040; overflow: hidden;";
+  _ndiv.id = "logo";
+  _ndiv.innerHTML = `
+  <svg aria-hidden="true" focusable="false" align='centre' width="94vh" height="94vh" data-prefix="fab"
+    data-icon="js-square" class="svg-inline--fa fa-js-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 448 512">
+    <path class="path" stroke="#f7df1d" stroke-width="6" stroke-linejoin="round" stroke-linecap="round"
+        stroke-miterlimit="10" stroke-dasharray="1720" stroke-dashoffset="1720" fill="none"
+        d="M343.6 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z">
+    </path>
+    <path class="path" stroke="#f7df1d" stroke-width="6" stroke-linejoin="round" stroke-linecap="round"
+        stroke-miterlimit="10" stroke-dasharray="900" stroke-dashoffset="900" fill="#404040"
+        d="M243.8 381.4c0 43.6-25.6 63.5-62.9 63.5-33.7 0-53.2-17.4-63.2-38.5l34.3-20.7c6.6 11.7 12.6 21.6 27.1 21.6 13.8 
+        0 22.6-5.4 22.6-26.5V237.7h42.1v143.7zm99.6 63.5c-39.1 0-64.4-18.6-76.7-43l34.3-19.8c9 14.7 20.8 25.6 41.5 25.6 17.4
+        0 28.6-8.7 28.6-20.8 0-14.4-11.4-19.5-30.7-28l-10.5-4.5c-30.4-12.9-50.5-29.2-50.5-63.5 0-31.6 24.1-55.6 61.6-55.6 26.8
+        0 46 9.3 59.8 33.7L368 290c-7.2-12.9-15-18-27.1-18-12.3 0-20.1 7.8-20.1 18 0 12.6 7.8 17.7 25.9 25.6l10.5 4.5c35.8 15.3 
+        55.9 31 55.9 66.2 0 37.8-29.8 58.6-69.7 58.6z">
+    </path>
+  </svg>
+  `;
+  // Implementing children node
+  _main.appendChild(_div);
+  document.getElementsByClassName("maincontent")[0].appendChild(_h1);
+  document.getElementById("CSS").appendChild(_ndiv);
+
+  // Making introduction to JavaScript Active
+  let _nav = document.getElementsByClassName("navbutton")[0];
+  _nav.className = "navbutton active";
+};
+
+/**
  * Creating main section
  */
 const _startNavigation = () => {
@@ -3023,6 +3065,7 @@ const _mainContentRendered = (content) => {
     _WhatNext,
   ];
   if (content == _navMenu[0]) {
+    _landupContent(content);
   } else {
     let _main = document.getElementsByClassName("main")[0];
     let _div = document.createElement("div");
@@ -3046,7 +3089,6 @@ const _mainContentRendered = (content) => {
         break;
       }
     }
-    console.log("------->", _conditionCall);
     document.getElementById(`pre`).appendChild(_conditionCall());
   }
 };
@@ -3102,8 +3144,8 @@ const _copyright = () => {
 const startApplication = () => {
   _startNavigation();
   setTimeout(() => {
-    // _landupContent();
     _createNavigation();
+    _landupContent();
     _copyright();
   }, 10);
 };
