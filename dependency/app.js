cb6699cbe3c0b5e7676cf306083209ffaf07a1d6
Hariom <harim987316@gmail.com>
2021-12-29 20:32:40 +0530

    code check-in

--- dependency/app.js
+++ dependency/app.js
@@ -3234,8 +3234,134 @@ const startApplication = () => {
   }, 100);
 };
 
-startApplication();
-
 /**
  * Created and developed by @hari from scractch to production
  */
+
+const refreshbody = (element) => {
+  if (element == "preloader") {
+    document.getElementById("body").remove();
+    document.getElementById("longfazers").remove();
+    document.getElementById("preloaderText").remove();
+  } else if (element == "loader") {
+    setTimeout(() => {
+      document.getElementById("loader").remove();
+    }, 5000);
+  }
+};
+
+/************ Adding code for loading page ****************/
+const preloader = () => {
+  /**
+   * Creating the body
+   */
+  const _createbody = () => {
+    let _body = document.body;
+    let _bodydiv = document.createElement("div");
+    _bodydiv.id = "body";
+    _bodydiv.className = "body";
+    let _span = document.createElement("span");
+    _span.id = "main";
+    let _basediv = document.createElement("div");
+    _basediv.className = "base";
+    _basediv.id = "base";
+
+    // Implementing the body DOM
+    _body.appendChild(_bodydiv);
+    document.getElementById("body").appendChild(_span);
+    document.getElementById("body").appendChild(_basediv);
+    // Implementing the main span dom
+    for (let i = 0; i < 4; i++) {
+      let _nullspan = document.createElement("span");
+      _nullspan.id = `mainspan${i}`;
+      document.getElementById("main").appendChild(_nullspan);
+    }
+    // Implementing the base dom
+    let _nullspan = document.createElement("span");
+    _nullspan.id = "basespan";
+    document.getElementById("base").appendChild(_nullspan);
+    let _facediv = document.createElement("div");
+    _facediv.className = "face";
+    document.getElementById("base").appendChild(_facediv);
+  };
+  /**
+   * Creating the Background
+   */
+  const _backbody = () => {
+    let _body = document.body;
+    let _longfazersdiv = document.createElement("div");
+    _longfazersdiv.id = "longfazers";
+    _longfazersdiv.className = "longfazers";
+
+    // Implemeting the body dom
+    _body.appendChild(_longfazersdiv);
+    for (let i = 0; i < 4; i++) {
+      let _nullspan = document.createElement("span");
+      _nullspan.id = `longspan${i}`;
+      document.getElementById("longfazers").appendChild(_nullspan);
+    }
+  };
+  /**
+   * Creating text
+   */
+  const _bodytext = () => {
+    let _body = document.body;
+    let _h1 = document.createElement("h1");
+    _h1.id = "preloaderText";
+    _h1.innerHTML = "Created and developer by @hariomgola (github)";
+    _body.appendChild(_h1);
+  };
+  const startpreLoader = () => {
+    _createbody();
+    _bodytext();
+    setTimeout(() => {
+      _backbody();
+    }, 50);
+  };
+  startpreLoader();
+};
+
+/**
+ * loader functionality code
+ */
+const _createLoader = () => {
+  let _body = document.body;
+  let _loaderdiv = document.createElement("div");
+  _loaderdiv.id = "loader";
+  _loaderdiv.className = "loader";
+  _body.appendChild(_loaderdiv);
+  for (let i = 0; i < 5; i++) {
+    let _loadernesteddiv = document.createElement("div");
+    _loadernesteddiv.id = `loadertile${i}`;
+    _loadernesteddiv.className = "loadertile";
+    document.getElementById("loader").appendChild(_loadernesteddiv);
+  }
+};
+
+const _workerfunction = () => {
+  setTimeout(() => {
+    let _loader = document.getElementById("loader");
+    _loader.className = "loader loader--active";
+    setTimeout(() => {
+      _loader.classList = "loader";
+    }, 1500);
+  }, 1000);
+};
+
+/**
+ * Main working functionality
+ */
+(function () {
+  preloader();
+  setTimeout(() => {
+    _createLoader();
+    _workerfunction();
+    setTimeout(() => {
+      refreshbody("preloader");
+    }, 2000);
+    setTimeout(() => {
+      startApplication();
+      refreshbody("loader");
+    }, 2200);
+  }, 4000);
+})();
